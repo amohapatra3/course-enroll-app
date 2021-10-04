443c82bef610515aa61b001eb5ed35994ddace38
Anupras Mohapatra <amohapatra3@wisc.edu>
2021-10-03 23:09:52 -0500

    course and section

--- src/Course.js
+++ src/Course.js
@@ -1,19 +1,62 @@
-import React from 'react';
-import './App.css';
-import Section from './Section'
+import React from "react";
+import { Accordion } from "react-bootstrap";
+import "./App.css";
+import Section from "./Section";
 
 class Course extends React.Component {
-  
-
-
+  requisites() {
+    let requisite = "";
+    let j = 0;
+    if (this.props.data.requisites.length === 0) {
+      return "None";
+    } else if (
+      this.props.data.requisites.length === 1 &&
+      this.props.data.requisites[0].length === 1
+    ) {
+      return this.props.data.requisites[0];
+    } else {
+      this.props.data.requisites.forEach((element) => {
+        for (let index = 0; index < element.length; index++) {
+          let req = "(" + this.props.data.requisites[j][index];
+          if (index < this.props.data.requisites[j].length - 1) {
+            requisite += req + " OR ";
+          } else {
+            requisite += req + ")";
+          }
+        }
+        if (
+          this.props.data.requisites.length > 1 &&
+          j !== this.props.data.requisites.length - 1
+        ) {
+          requisite += " AND ";
+        }
+        j++;
+      });
+    }
+    return requisite;
+  }
 
   render() {
     return (
       <div>
-
+        <Accordion>
+          <Accordion.Item eventKey={this.props.data.number}>
+            <Accordion.Header>
+              {this.props.data.number}: {this.props.data.name}
+            </Accordion.Header>
+            <Accordion.Body>
+              Credits: {this.props.data.credits} <br /> <br />
+              Description: {this.props.data.description} <br /> <br />
+              Requisites: {this.requisites()} <br /> <br />
+              Keywords: {this.props.data.keywords.join()} <br /> <br />
+              Subject: {this.props.data.subject} <br />
+              <br />
+              <Section sections={this.props.data.sections} />
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </div>
-    )
+    );
   }
 }
-
 export default Course;
--- src/Section.js
+++ src/Section.js
@@ -1,19 +1,54 @@
-import React from 'react'
-import './App.css'
-import Subsection from './Subsection.js'
+import React from "react";
+import "./App.css";
+import Subsection from "./Subsection.js";
 
 class Section extends React.Component {
+  instructors() {
+    let instructor = [];
+    this.props.sections.forEach((element) => {
+      instructor.push(element.instructor);
+    });
+    return instructor;
+  }
+  number() {
+    let courseNumber = [];
+    this.props.sections.forEach((element) => {
+      courseNumber.push(element.number);
+    });
+    return courseNumber;
+  }
+  getKeys() {
+    return Object.keys(this.props.sections);
+  }
+  getValues() {
+    return Object.values(this.props.sections);
+  }
 
-
-
-
-	render() {
-		return (
-			<div>
-				
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <h1> Sections </h1>
+        {this.props.sections.map((key, index) => (
+          <ul>
+            <li> Section Number: {key.number} </li>
+            <li>Instructor: {key.instructor} </li>
+            <li>Location: {key.location} </li>
+            <li> Meeting Times: </li>
+            {Object.keys(key.time).map((obj, i) => {
+              return (
+                <ul>
+                  <li>
+                    {" "}
+                    {obj} {key.time[obj]}
+                  </li>
+                </ul>
+              );
+            })}
+          </ul>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Section;
