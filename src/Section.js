e2a3b1248f0b24b51f391bc2bc031ff98e13e166
Anupras Mohapatra <amohapatra3@wisc.edu>
2021-10-04 14:16:57 -0500

    subsections

--- src/Section.js
+++ src/Section.js
@@ -6,27 +6,30 @@ class Section extends React.Component {
   render() {
     return (
       <div>
-        <h1> Sections </h1>
         {this.props.sections.map((key, index) => (
-          <ul>
-            <li> Section Number: {key.number} </li>
-            <li>Instructor: {key.instructor} </li>
-            <li>Location: {key.location} </li>
-            <li> Meeting Times: </li>
-            {Object.keys(key.time).map((obj, i) => {
-              return (
-                <ul>
-                  <li>
-                    {" "}
-                    {obj} {key.time[obj]}
-                  </li>
-                </ul>
-              );
-            })}
-          </ul>
+          <>
+            <h1> Section </h1>
+            <ul>
+              <li key={key.number}> Section Number: {key.number} </li>
+              <li key={key.instructor}>Instructor: {key.instructor} </li>
+              <li key={key.location}>Location: {key.location} </li>
+              <li> Meeting Times: </li>
+              {Object.keys(key.time).map((obj, i) => {
+                return (
+                  <ul>
+                    <li key={obj}>
+                      {" "}
+                      {obj} {key.time[obj]}
+                    </li>
+                  </ul>
+                );
+              })}
+              <h2> Subsections</h2>
+              {key.subsections.length === 0 ? <span>None</span> : null}
+              <Subsection data={this.props.sections} />
+            </ul>
+          </>
         ))}
-        <h2> Subsections</h2>
-        <Subsection subsection={this.props.sections.subsections} />
       </div>
     );
   }
--- src/Subsection.js
+++ src/Subsection.js
@@ -1,19 +1,39 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
 class Subsection extends React.Component {
-
-
-
-	
-
-	render() {
-		return (
-			<div>
-				
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        {this.props.data.map((key, index) => (
+          <>
+            {Object.values(key.subsections).map((obj, i) => {
+              return (
+                <>
+                  <ul>
+                    <li key={obj.number}> Number: {obj.number}</li>
+                    <li key={obj.location}> Location: {obj.location}</li>
+                    <li> Meeting Times: </li>
+                    {Object.keys(obj.time).map((day, j) => {
+                      return (
+                        <ul>
+                          <li key={day}>
+                            {" "}
+                            {day} {obj.time[day]}
+                          </li>
+                        </ul>
+                      );
+                    })}
+                  </ul>
+                  <br />
+                </>
+              );
+            })}
+          </>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Subsection;
