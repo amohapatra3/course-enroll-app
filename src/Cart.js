c32f0da874a5a4bd39601991ac3d7367c550d1f8
Anupras Mohapatra <amohapatra3@wisc.edu>
2021-10-07 09:40:05 -0500

    cart almost done?

---
+++ src/Cart.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Course from "./Course";
+import CourseArea from "./CourseArea";
+import { Accordion } from "react-bootstrap";
+import Section from "./Section";
+class Cart extends React.Component {
+  removeFromCart(item) {
+    this.props.callbackFromCart(
+      this.props.cart.filter(
+        (element) => element.courseNumber !== item.courseNumber
+      )
+    );
+  }
+  render() {
+    return (
+      <div>
+        <h1> Cart </h1>
+        {this.props.cart.map((item, i) => {
+          return (
+            <>
+              <p key={item.courseNumber}>
+                {item.courseNumber} {item.courseName}
+              </p>
+              <p key={item.instructor}>{item.instructor}</p>
+              <p key={item.location}>{item.location}</p>
+              <p key={item.number}>{item.number}</p>
+
+              {Object.keys(item.time).map((day, j) => {
+                return (
+                  <ul>
+                    <li key={day}>
+                      {" "}
+                      {day} {item.time[day]}
+                    </li>
+                  </ul>
+                );
+              })}
+              <button onClick={() => this.removeFromCart(item)}>
+                {" "}
+                Remove from cart
+              </button>
+            </>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default Cart;
