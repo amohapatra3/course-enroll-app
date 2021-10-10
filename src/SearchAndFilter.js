6e742bfed53f0a382a1d2b5a1470795b6ec72c99
Anupras Mohapatra <amohapatra3@wisc.edu>
2021-10-09 22:44:48 -0500

    some updates for search and filter

--- src/SearchAndFilter.js
+++ src/SearchAndFilter.js
@@ -1,14 +1,19 @@
 class SearchAndFilter {
   searchAndFilter(courses, search, subject, minimumCredits, maximumCredits) {
-    if (
-      subject === "All" &&
-      ((minimumCredits === "" && maximumCredits === "") ||
-        minimumCredits === "" ||
-        maximumCredits === "")
-    ) {
+    if (subject === "All" && minimumCredits === "" && maximumCredits === "") {
       return courses.filter((course) =>
         course.keywords.join().includes(search.trim())
       );
+    }
+    if (subject === "All" && minimumCredits !== "" && maximumCredits === "") {
+      return courses
+        .filter((course) => course.keywords.join().includes(search.trim()))
+        .filter((course) => course.credits >= parseInt(minimumCredits));
+    }
+    if (subject === "All" && minimumCredits === "" && maximumCredits !== "") {
+      return courses
+        .filter((course) => course.keywords.join().includes(search.trim()))
+        .filter((course) => course.credits <= parseInt(maximumCredits));
     }
     if (subject === "All" && search === "") {
       return courses.filter(
@@ -26,20 +31,20 @@ class SearchAndFilter {
             course.credits <= parseInt(maximumCredits)
         );
     }
-    if (
-      search === "" &&
-      ((minimumCredits === "" && maximumCredits === "") ||
-        minimumCredits === "" ||
-        maximumCredits === "")
-    ) {
+    if (search === "" && minimumCredits === "" && maximumCredits === "") {
       return courses.filter((course) => course.subject === subject);
     }
-
-    if (
-      (minimumCredits === "" && maximumCredits === "") ||
-      minimumCredits === "" ||
-      maximumCredits === ""
-    ) {
+    if (search === "" && minimumCredits === "" && maximumCredits !== "") {
+      return courses
+        .filter((course) => course.subject === subject)
+        .filter((course) => course.credits <= parseInt(maximumCredits));
+    }
+    if (search === "" && minimumCredits !== "" && maximumCredits === "") {
+      return courses
+        .filter((course) => course.subject === subject)
+        .filter((course) => course.credits >= parseInt(minimumCredits));
+    }
+    if (minimumCredits === "" && maximumCredits === "") {
       return courses
         .filter((course) => course.keywords.join().includes(search.trim()))
         .filter((course) => course.subject === subject);
@@ -52,6 +57,12 @@ class SearchAndFilter {
             course.credits >= parseInt(minimumCredits) &&
             course.credits <= parseInt(maximumCredits)
         );
+    }
+    if (minimumCredits === "" && maximumCredits !== "") {
+      courses.filter((course) => course.credits <= parseInt(maximumCredits));
+    }
+    if (maximumCredits === "" && minimumCredits !== "") {
+      courses.filter((course) => course.credits >= parseInt(minimumCredits));
     }
     return courses
       .filter((course) => course.subject === subject)
